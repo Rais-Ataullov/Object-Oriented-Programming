c95fc0fdb33ae5d933556ae9f6f832cb9d1d1130
Rais-Ataullov <ataullov2004@gmail.com>
2023-09-30 12:54:06 +0400

    Update Interface

--- LabWork_2/functions/MathFunction.java
+++ LabWork_2/functions/MathFunction.java
@@ -3,4 +3,10 @@ package functions;
 interface MathFunction
 {
     double apply(double x);
+
+    default CompositeFunction andThen(MathFunction afterFunction)
+    {
+        CompositeFunction newFunction = new CompositeFunction(this, afterFunction);
+        return newFunction;
+    };
 }
